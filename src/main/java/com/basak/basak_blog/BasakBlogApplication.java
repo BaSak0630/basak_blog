edc667bcb3d215cce523ad9471a83f97a0ebd15f
BaSak0630 <babamba125@gmail.com>
2024-12-27 15:56:02 +0900

    init

---
+++ src/main/java/com/basak/basak_blog/BasakBlogApplication.java
@@ -0,0 +1,13 @@
+package com.basak.basak_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasakBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasakBlogApplication.class, args);
+    }
+
+}
